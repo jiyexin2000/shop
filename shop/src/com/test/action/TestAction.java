aedf705cb9af77cc0290a8d80ea5139f6d5b77ff
Administrator <Administrator@windows10.microdone.cn>
2020-11-09 15:07:41 +0800

    提交testaction

--- shop/src/com/test/action/TestAction.java
+++ shop/src/com/test/action/TestAction.java
@@ -5,6 +5,7 @@ public class TestAction {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TestAction");
+		System.out.println("sdfsd");
 	}
 
 }
